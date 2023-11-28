5d0c96c580104c4459c955d3946491860a340113
creeds-knight <apedoarthur21@gmail.com>
2023-11-29 00:31:43 +0300

    corrections-task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,6 @@ else:
     new = number * -1
     y = (new % 10) * -1
     if (new % 10 == 0):
-        print(f"Last digit of {number} is {new} and is zero")
-    print(f"Last digit of {number} is {y} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {new} and is 0")
+    else:
+        print(f"Last digit of {number} is {y} and is less than 6 and not 0")
