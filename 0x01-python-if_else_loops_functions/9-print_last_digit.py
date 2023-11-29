d39e998ffcb67517f87778bdc073c71d9e71cc93
creeds-knight <apedoarthur21@gmail.com>
2023-11-29 16:39:54 +0300

    corrections-task9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,10 @@
 def print_last_digit(number):
     if number >= 0:
         x = number % 10
+        print(x, end="")
         return x
     else:
         new = number * -1
         y = (new % 10)
+        print(y, end="")
         return y
