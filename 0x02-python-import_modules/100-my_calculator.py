cfc18629af5fccfa164aa11ed2a55f58e27fedff
creeds-knight <apedoarthur21@gmail.com>
2023-12-02 17:14:59 +0300

    task-6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,3 +19,4 @@ if __name__ == "__main__":
             print(f"{a} - {b} = {div(a, b)}")
         else:
             print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
