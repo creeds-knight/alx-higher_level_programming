2aa6910fb515135253d30acf92c4ab2fb7e5fed2
creeds-knight <apedoarthur21@gmail.com>
2023-12-04 22:41:45 +0300

    task-1, corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx > len(my_list):
         return None
     else:
         return my_list[idx]
-
-
-if __name__ == "__main__":
-    element_at()
