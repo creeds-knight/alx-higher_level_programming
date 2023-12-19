6cf7c7955bdc0985ab6adf8c17625d08e6dae34c
creeds-knight <apedoarthur21@gmail.com>
2023-12-20 00:35:00 +0300

    corrections

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ A class to define a Square"""
+
+
 class Square:
     """ A representation of a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ A class to define a square"""
+
+
 class Square:
     """ An Instance to initialise the class with size attribute"""
     def __init__(self, size):
         self.__size = size
-
