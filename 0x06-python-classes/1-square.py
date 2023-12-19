144784a7acaf701d70d5a20fe46c21e65dccff3c
creeds-knight <apedoarthur21@gmail.com>
2023-12-19 23:45:57 +0300

    adding doc srings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 class Square:
+    """ A class that defines square"""
     def __init__(self, size):
+        """ A function to initialise Square instance with given size"""
         self.__size = size
+
