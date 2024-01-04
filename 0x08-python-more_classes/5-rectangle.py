69218c62c6ef634c81cd08c64b97d34c1993d3d7
creeds-knight <apedoarthur21@gmail.com>
2024-01-04 23:30:15 +0300

    task-5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+""" A class that defines a rectangle"""
+
+
+class Rectangle:
+    """ A representation of the rectangle"""
+
+    no_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+        Rectangle.no_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.__width * self.height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            return '\n'.join('#' * self.__width for i in range(self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.no_instances -= 1
