1f7a2c3889b11bf2de0ccf77ecf0c436a8887a4e
creeds-knight <apedoarthur21@gmail.com>
2024-01-16 14:22:13 +0300

    task3-update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
         self.width = width
         self.height = height
         self.x = x
-        self.   y = y
+        self.y = y
 
     @property
     def width(self):
@@ -46,7 +46,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -58,7 +58,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
