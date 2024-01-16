016ee76cf3c1ec67a2686717fa8b11e4a40658ee
creeds-knight <apedoarthur21@gmail.com>
2024-01-16 15:51:08 +0300

    task17-update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,7 +46,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns the list representation of a json string"""
-        if json_string is None or len(json_string) == 0:
+        if json_string is None and len(json_string) == 0:
             return "[]"
         return json.loads(json_string)
 
