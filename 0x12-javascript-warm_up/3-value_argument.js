dbb80aab03bf5daa56fc98ae550973f5f5c124f6
creeds-knight <apedoarthur21@gmail.com>
2024-03-11 17:44:33 +0300

    task-3, print first arg

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,6 @@
 const { argv } = require('node:process');
 if (argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(argv[2]);
 }
