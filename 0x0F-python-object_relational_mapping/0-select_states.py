8deda3104b1ffc290bfe19ce0b8bf7d11afd8f48
creeds-knight <apedoarthur21@gmail.com>
2024-03-15 21:34:15 +0300

    task-0, querring states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,19 +3,24 @@
     A script that lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
+import sys
 
 
-def states():
+def states(username, password, db_name):
     """ A function to print out the states"""
 
-    db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                         passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states")
+    cur.execute("SELECT id, name FROM states WHERE id IN(SELECT MIN(id)
+                FROM states GROUP BY name) ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
 
 
 if __name__ == "__main__":
-    states()
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <database_name>")
+    username, passwd, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    states(username, passwd, db_name)
