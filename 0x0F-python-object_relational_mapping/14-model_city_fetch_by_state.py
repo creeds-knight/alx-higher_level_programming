d385e9e9af50466d000b894dc2c7a886bbf59215
creeds-knight <apedoarthur21@gmail.com>
2024-03-17 02:59:45 +0300

    task-14, join

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" A module that lists all State Objects from hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from model_city import City
+import sys
+
+
+def do_join(username, password, db_name):
+    """ A function to return objects"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+            username, password, "3306", db_name))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        cit_st = session.query(City, State).
+        join(State, City.state_id == State.id).order_by(City.id).all()
+        for city, state in cit_st:
+            print(f"{state.name}: ({city.id}) {city.name}")
+
+        session.close()
+    except SQLAlchemyError as e:
+        print("Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <database name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1:]
+    do_join(username, password, db_name)
