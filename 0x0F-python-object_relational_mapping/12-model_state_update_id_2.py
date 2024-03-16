fed6a47076a9b4f6d8a74b20553c76220a710942
creeds-knight <apedoarthur21@gmail.com>
2024-03-17 01:02:03 +0300

    Task-12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" A module that contains change_name function
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+import sys
+
+
+def change_name(username, password, db_name):
+    """ A function to add a state"""
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format(
+            username, password, "3306", db_name))
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        states = session.query(State).order_by(State.id).filter(
+                State.id == 2)
+        for state in states:
+              state.name = "New Mexico"
+        session.commit()
+        session.close()
+    except SQLAlchemyError as e:
+        print("Error:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: <username> <password> <database name>")
+        sys.exit(1)
+
+    username, password, db_name = sys.argv[1:]
+    change_name(username, password, db_name)
