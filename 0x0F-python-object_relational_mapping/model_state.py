abcf0120a01fffa3e8a8115c65e9ee6dd8ebc28a
creeds-knight <apedoarthur21@gmail.com>
2024-03-16 19:26:01 +0300

    task-6, sqlachemy intro

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,16 +7,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format("root",\
-        "root", "3306", "my_db"), pool_pre_ping=True)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'.format("root",
+                       "root", "3306", "my_db"), pool_pre_ping=True)
 
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-
