3634b3de80ca3685b0ef7a82c9d446b598b32d8b
creeds-knight <apedoarthur21@gmail.com>
2024-03-31 14:49:49 +0300

    post

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """ Fetches https://alx-intranet.hbtn.io/status using urllib package"""
+
 if __name__ == "__main__":
     import urllib.request
-
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
     print("Body response:")
     print("\t- type: {}".format(type(html)))
-    print("\t- type: {}".format(html))
-    print("\t- type: {}".format(html.decode('utf-8')))
-
-
-
-
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
