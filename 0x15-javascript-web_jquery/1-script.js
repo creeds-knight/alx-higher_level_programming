2ae3edd84a1f280991d8dd85b9aae6c1260d597a
creeds-knight <apedoarthur21@gmail.com>
2024-05-02 23:02:52 +0300

    task2

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// A script that updates the color of the header element to red
+
+const header = document.querySelector('header');
+
+$('header').css('color', '#FF0000');
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// updates the text color of header to red when user clicks
+
+$('DIV#red_header').click(function () {
+	$('header').css('color', '#FF0000');
+});
